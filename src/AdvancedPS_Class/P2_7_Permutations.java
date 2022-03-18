a5ed288f0c323b99d4b19af285db6f1eb9b2d1cd
WhalesBob <ndw8200@naver.com>
2022-03-18 22:30:25 +0900

    add explaination of makePermutation method

--- src/AdvancedPS_Class/P2_7_Permutations.java
+++ src/AdvancedPS_Class/P2_7_Permutations.java
@@ -27,18 +27,20 @@ public class P2_7_Permutations {
         }
         return array;
     }
-    static void makePermutation(Integer[] plusArray, ArrayList<Integer> array, int howManyLeft,int index){
-
-        if(howManyLeft == 0){
-            if(!includeInList(plusArray)){
-                permutationArray.add(plusArray);
+    static void makePermutation(Integer[] beingMadeArray, ArrayList<Integer> intArray, int leftElement, int index){
+        // 맨 처음에 부를 때는 beingMadeArray 를 main 에서 빈 Integer 배열을 주고, 계속 재귀함수를 부르며 채워가는 식입니다.
+        if(leftElement == 0){ // beingMadeArray 다 채웠을 때
+            if(!includeInList(beingMadeArray)){ // permutationArray에 안 포함되어 있으면 추가! 포함되있으면 겹치는 것이므로 X
+                permutationArray.add(beingMadeArray);
             }
         }else{
-            for(int i = 0; i < howManyLeft; i++){
-                ArrayList<Integer> newList = (ArrayList<Integer>)array.clone();
-                Integer[] subArray = plusArray.clone();
-                subArray[index] = newList.remove(i);
-                makePermutation(subArray,newList,(howManyLeft-1),(index+1));
+            for(int i = 0; i < leftElement; i++){ // leftElement는 결국 재귀함수가 불림에 따라서 계속 줄어듭니다.
+                ArrayList<Integer> newList = (ArrayList<Integer>)intArray.clone(); // 혹시나 intArray가 영향을 받으면 안되니 clone 해놓고
+                // 걔에서 줄입니다. 결국 for 문을 돌면서 intArray 가 직접적인 영향을 안 받게 했습니다.
+                Integer[] subArray = beingMadeArray.clone();  // subArray 도 clone 처리해서 for 문 돌때마다 하나씩 생성되게 했습니다.
+                subArray[index] = newList.remove(i); // newList 에서 하나 빼서 주기.
+                makePermutation(subArray,newList,(leftElement-1),(index+1)); // 결국 clone 되어 있는 애가 다 들어가면서 재귀함수를 돌립니다.
+                // leftElement 도 재귀를 돌면서 하나씩 줄어들고, 결국에는 0이 되도록 하며, index 도 계속 다음 index를 받도록 해야 합니다.
             }
         }
     }
