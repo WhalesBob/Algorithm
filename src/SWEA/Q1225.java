9bc701d2b05b12da034f8d26a41875f6f1b9b90c
WhalesBob <ndw8200@naver.com>
2022-09-06 10:42:20 +0900

    Solving SWEA Q1225

---
+++ src/SWEA/Q1225.java
@@ -0,0 +1,53 @@
+package SWEA;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Q1225 {
+    public static void main(String[] args) throws InterruptedException {
+        Scanner sc = new Scanner(System.in);
+        int T = 10;
+        int numbers = 8;
+        int n;
+
+        for(int test_case = 1; test_case <= T; test_case++){
+            Queue queue = new Queue();
+            sc.nextInt();
+
+            for(int i = 0; i < numbers; i++){
+                queue.enqueue(sc.nextInt());
+            }
+
+            int index = 0;
+            do{
+                n = queue.dequeue() - (index++ % 5 + 1);
+                n = Math.max(n, 0);
+                queue.enqueue(n);
+            }while(n != 0);
+
+            System.out.printf("#%d ",test_case);
+            for(int i = 0; i < 8; i++){
+                System.out.printf("%d ",queue.dequeue());
+            }
+            System.out.println("");
+        }
+    }
+}
+class Queue{
+
+    LinkedList<Integer> queue;
+
+    public Queue() {
+        queue = new LinkedList<>();
+    }
+
+    void enqueue(int n){
+        queue.addLast(n);
+    }
+
+    int dequeue(){
+        int n = queue.get(0);
+        queue.removeFirst();
+        return n;
+    }
+}
