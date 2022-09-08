a2c822f8a70250e2cccbc7d51d3744729024927a
WhalesBob <ndw8200@naver.com>
2022-09-08 16:46:34 +0900

    Solving SWEA Q1232~33

--- src/SWEA/Q1233.java
+++ src/SWEA/Q1233.java
@@ -2,48 +2,48 @@ package SWEA;
 
 import java.util.Scanner;
 
-public class Q1232 {
+public class Q1233 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int T = 10;
 
         for(int test_case = 1; test_case <= T; test_case++){
             int nodeCount = sc.nextInt();
-            Node32[] nodes = new Node32[nodeCount];
+            Node33[] nodes = new Node33[nodeCount];
 
             String[][] inputs = new String[nodeCount][];
             sc.next();
             for(int i = 0; i < nodeCount; i++){
                 inputs[i] = sc.nextLine().split(" ");
-                nodes[i] = new Node32("");
+                nodes[i] = new Node33("");
             }
 
-            for(int i = 0; i < nodeCount; i++){
+            for(int i = 0; i < nodeCount; i++) {
                 nodes[i].setData(inputs[i][1]);
-                if(inputs[i].length >= 3){
+                if (inputs[i].length >= 3) {
                     nodes[i].leftChild = nodes[Integer.parseInt(inputs[i][2]) - 1];
                 }
-                if(inputs[i].length == 4){
+                if (inputs[i].length == 4) {
                     nodes[i].rightChild = nodes[Integer.parseInt(inputs[i][3]) - 1];
                 }
             }
-            nodes[0].calculate();
-            System.out.printf("#%d %d\n",test_case,nodes[0].n);
+            System.out.printf("#%d %d\n",test_case,nodes[0].isValid() ? 1 : 0);
         }
     }
 }
-class Node32{
+class Node33{
     String data;
     int n;
-    Node32 leftChild;
-    Node32 rightChild;
+    Node33 leftChild;
+    Node33 rightChild;
 
-    public Node32(String data) {
+    public Node33(String data) {
         this.data = data;
         this.leftChild = null;
         this.rightChild = null;
         this.n = 0;
     }
+
     public void setData(String s){
         try{
             this.data = s;
@@ -52,30 +52,14 @@ class Node32{
             this.n = 0;
         }
     }
-    public void calculate(){
+    boolean isValid(){
+        boolean isLeftValid = true, isRightValid = true;
         if(this.leftChild != null){
-            this.leftChild.calculate();
+            isLeftValid = this.leftChild.isValid();
         }
         if(this.rightChild != null){
-            this.rightChild.calculate();
+            isRightValid = this.rightChild.isValid();
         }
-
-        try{
-            switch(data){
-                case "+":
-                    this.n = this.leftChild.n + this.rightChild.n;
-                    break;
-                case "-":
-                    this.n = this.leftChild.n - this.rightChild.n;
-                    break;
-                case "*":
-                    this.n = this.leftChild.n * this.rightChild.n;
-                    break;
-                case "/":
-                    this.n = this.leftChild.n / this.rightChild.n;
-            }
-        }catch(NullPointerException e){
-            return;
-        }
+        return isLeftValid && isRightValid && !(this.n == 0 && (this.leftChild == null || this.rightChild == null));
     }
 }
